78e549ed7fe975cc0a518db3e800c045be57b1f8
ShowHiddenCT <2923172249@qq.com>
2022-04-17 11:53:49 +0800

    添加模型

---
+++ WebApplication3/Models/SelectTeacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class SelectTeacher
+    {
+        public string TeacherNo { get; set; }
+        public string TeacherName { get; set; }
+        public string TeacherSex { get; set; }
+        public string DeptName { get; set; }
+        public string TeacherId { get; set; }
+        public string TeacherBirthday { get; set; }
+        public string TeacherTel { get; set; }
+        public string TeacherTitle { get; set; }
+    }
+}
