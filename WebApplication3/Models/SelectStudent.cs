25053afc618a49f7134767183a65d47bb2c96fc7
ShowHiddenCT <2923172249@qq.com>
2022-04-17 11:42:01 +0800

    选择

---
+++ WebApplication3/Models/SelectStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class SelectStudent
+    {
+        public string StudentNo { get; set; }
+        public string StudentName { get; set; }
+        public string StudentSex { get; set; }
+        public string Major { get; set; }
+        public string College { get; set; }
+        public string StudentId { get; set; }
+        public string StudentBirthday { get; set; }
+        public string StudentTel { get; set; }
+    }
+}
