a82552a148a88bd3818f9b84ab89046562294ae5
zhouchengshan <877233230@qq.com>
2022-05-11 20:12:16 +0800

    学院

---
+++ WebApplication3/Areas/dept/deptAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Areas.dept
+{
+    public class deptAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "dept";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "dept_default",
+                "dept/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
