16c23a56361d4c264d502a5420cd3af3f822ed69
zhouchengshan <877233230@qq.com>
2022-06-01 20:15:09 +0800

    选课

---
+++ WebApplication3/Areas/stucourse/stucourseAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Areas.stucourse
+{
+    public class stucourseAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "stucourse";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "stucourse_default",
+                "stucourse/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
